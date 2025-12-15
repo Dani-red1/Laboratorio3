808e2ab3b488b2deec2d3b8461940ed9649adb02
Daniel <233632326+Dani-red1@users.noreply.github.com>
2025-12-15 14:01:01 -0600

    Se agrega EstadisticasViewModel como contenedor de datos que transfiere todas los valores del inventario desde el controlador a la vista

---
+++ ViewModels/EstadisticasViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public class EstadisticasViewModel
+{
+    public decimal PromedioPrecios { get; set; }
+    public decimal ValorTotalInventario { get; set; }
+    public List<Producto> ProductosMasCaros { get; set; }
+    public List<Producto> StockCritico { get; set; }
+}
