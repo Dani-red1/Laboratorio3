dd91e99bd5d2ca7f53e84b87b12dade4333d7804
Daniel <233632326+Dani-red1@users.noreply.github.com>
2025-12-15 14:09:00 -0600

    Se agrega views Productos, que contiene las opciones de crear, eliminar, editar y el index con los formularios para crear, editar y eliminar producto.

---
+++ Views/Productos/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Techsolutions.Views.Productos
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
